d664db048e132c6fc5902eab58dae73a7f13585e
Guilherme Moraes <guilherme.moraes@ic.oneforce.com.br>
2016-10-25 18:49:06 -0200

    Implementaçao da resposta enviada pelo Watson (incompleto)

---
+++ app/src/main/java/com/hop/nami/Activity/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.hop.nami.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Tiago on 25/10/2016.
+ */
+
+public class SplashActivity extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
